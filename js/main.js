45cffb2b22b1723dc3009d384c07a00baf5b7556
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2024-09-21 15:09:05 +0530

    refactor

--- js/main.js
+++ js/main.js
@@ -2,15 +2,9 @@ const canvas = document.getElementById("screen");
 /** @type {WebGL2RenderingContext} */
 const gl = canvas.getContext("webgl2");
 
-self.onload = () => {
-    canvas.width = self.innerWidth;
-    canvas.height = self.innerHeight;
+window.onload = () => {
+    main();
 };
-
-self.addEventListener("resize", () => {
-    canvas.width = self.innerWidth;
-    canvas.height = self.innerHeight;
-});
 
 class Shader {
     #id;
@@ -55,21 +49,25 @@ class Shader {
         return this.#locations.get(name);
     }
 
+    get_attribute(name) {
+        return this.#get_uniform_location(name);
+    }
+
     upload_mat4(name, mat4) {
         gl.uniformMatrix4fv(this.#get_uniform_location(name), false, mat4);
     }
 }
 
 const vert = `#version 300 es
-in vec3 pos;
+in vec3 position;
 uniform mat4 model;
 uniform mat4 projection;
 
 out vec3 color;
 
 void main() {
-    color = pos;
-    gl_Position = projection * model * vec4(pos, 1);
+    color = position;
+    gl_Position = projection * model * vec4(position, 1);
 }
 `;
 
@@ -85,67 +83,150 @@ void main() {
 }
 `;
 
-const shader = new Shader(vert, frag);
+class GenericBuffer {
+    #id;
+    #kind;
 
-const positions = [
-    -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1,
-    -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1,
-];
-const position_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    constructor(kind) {
+        this.#id = gl.createBuffer();
+        this.#kind = kind;
+    }
 
-const indices = [
-    1, 2, 0, 2, 3, 0, 6, 2, 1, 1, 5, 6, 6, 5, 4, 4, 7, 6,
-    6, 3, 2, 7, 3, 6, 3, 7, 0, 7, 4, 0, 5, 1, 0, 4, 5, 0
-];
-const index_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-    new Uint16Array(indices), gl.STATIC_DRAW);
+    bind() {
+        gl.bindBuffer(this.#kind, this.#id);
+    }
 
-let last_time = 0;
-let cube_rotation = 0;
-function update(time) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clearDepth(1.0);
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
+    unbind() {
+        gl.bindBuffer(this.#kind, 0);
+    }
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    let delta_time = time - last_time;
-    const field_of_view = 45 * Math.PI / 180;
-    const aspect = canvas.width / canvas.height;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const mat4 = glMatrix.mat4;
-    const projection = mat4.create();
-
-    cube_rotation += (delta_time * 0.001) % 360;
-
-    mat4.perspective(projection,
-        field_of_view,
-        aspect,
-        zNear,
-        zFar);
-
-    const model = mat4.create();
-    mat4.translate(model, model, [0.0, 0.0, -6.0]);
-    mat4.rotate(model, model, cube_rotation, [0, 0, 1]);
-    mat4.rotate(model, model, cube_rotation * 0.7, [0, 1, 0]);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(0);
-    shader.bind();
-    shader.upload_mat4("projection", projection);
-    shader.upload_mat4("model", model);
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-    requestAnimationFrame(update);
-    last_time = time;
+    upload_data(data) {
+        this.bind();
+        gl.bufferData(this.#kind, data, gl.STATIC_DRAW);
+    }
 }
 
-update(0);
+class Batch {
+    #shader;
+    #id;
+    #buffers;
+    #index_buffer;
+    #has_indices;
+    #count;
+
+    constructor(shader, positions, indices = []) {
+        this.#shader = shader;
+        this.#id = gl.createVertexArray();
+        gl.bindVertexArray(this.#id);
+        this.#buffers = [];
+        this.add_buffer("position", positions, 3);
+        this.#has_indices = indices.length !== 0;
+        this.#index_buffer = undefined;
+        if (this.#has_indices) {
+            this.#index_buffer = new GenericBuffer(gl.ELEMENT_ARRAY_BUFFER);
+            this.#index_buffer.bind();
+            this.#index_buffer.upload_data(new Uint16Array(indices));
+        }
+        gl.bindVertexArray(null);
+        this.#count = this.#has_indices ? indices.length : (positions.length / 3);
+    }
+
+    add_buffer(name, data, size) {
+        const buffer = new GenericBuffer(gl.ARRAY_BUFFER);
+        buffer.bind();
+        const buf = new Float32Array(data);
+        buffer.upload_data(buf);
+        const attr = this.#shader.get_attribute(name);
+        gl.enableVertexAttribArray(attr);
+        gl.vertexAttribPointer(attr, size, gl.FLOAT, false, 0, 0);
+        this.#buffers.push(buffer);
+    }
+
+    draw() {
+        gl.bindVertexArray(this.#id);
+        gl.drawElements(gl.TRIANGLES, this.#count, gl.UNSIGNED_SHORT, 0);
+        gl.bindVertexArray(null);
+    }
+}
+
+class MainLoop {
+    #shader;
+    #batch;
+    #projection;
+    #model;
+    #rotation
+    #last_time;
+
+    constructor() {
+        this.#shader = new Shader(vert, frag);
+        this.#projection = mat4.create();
+        this.#model = mat4.create();
+        this.#rotation = 0.0;
+        this.#last_time = 0;
+
+        const positions = [
+            -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1,
+            -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1,
+        ];
+        const indices = [
+            1, 2, 0, 2, 3, 0, 6, 2, 1, 1, 5, 6, 6, 5, 4, 4, 7, 6,
+            6, 3, 2, 7, 3, 6, 3, 7, 0, 7, 4, 0, 5, 1, 0, 4, 5, 0
+        ];
+        this.#batch = new Batch(this.#shader, positions, indices);
+
+        this.update_projection();
+        this.#update_model();
+    }
+
+    update_projection() {
+        mat4.perspective(
+            this.#projection,
+            toRadian(45.0),
+            canvas.width / canvas.height,
+            0.1, 100.0
+        );
+    }
+
+    #update_model() {
+        this.#model = mat4.create();
+        mat4.translate(this.#model, this.#model, [0.0, 0.0, -6.0]);
+        mat4.rotate(this.#model, this.#model, this.#rotation, [0, 0, 1]);
+        mat4.rotate(this.#model, this.#model, this.#rotation * 0.7, [0, 1, 0]);
+    }
+
+    update(time = 0) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        let delta_time = time - this.#last_time;
+        this.#rotation += (delta_time * 0.001) % 360;
+
+        this.#update_model();
+
+        this.#shader.bind();
+        this.#shader.upload_mat4("projection", this.#projection);
+        this.#shader.upload_mat4("model", this.#model);
+
+        gl.viewport(0, 0, canvas.width, canvas.height);
+        this.#batch.draw();
+        requestAnimationFrame(t => this.update(t));
+        this.#last_time = time;
+    }
+}
+
+const main = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const main_loop = new MainLoop();
+    main_loop.update();
+    window.addEventListener("resize", () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        main_loop.update_projection();
+    });
+};
